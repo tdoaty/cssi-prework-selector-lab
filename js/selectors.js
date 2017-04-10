d1418dd65889a5660b46e6a48a7d1bd9f906ee70
tdoaty <tdoaty@google.com>
2017-04-10 11:57:19 -0400

    selectors.js

--- js/selectors.js
+++ js/selectors.js
@@ -5,3 +5,20 @@ function pSelector(){
   return $('p');
 }
 */
+function h1Selector(){
+  return $('h1');
+}
+function liInOlSelector(){
+  return $('ol li');
+}
+function linkSelector(){
+  return $('#my-id .myclass a');
+}
+
+function catSelector(){
+  return $('img.alt ="cat sleeping"');
+}
+
+function checkboxInputSelector(){
+  return $(':checkbox');
+}
